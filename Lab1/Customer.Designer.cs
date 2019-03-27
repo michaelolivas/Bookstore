2bab99bb8a29b277114dbc23107bb19219fe0aa2
michaelolivas <michaelolivas66@gmail.com>
2019-03-26 20:23:12 -0700

    implement customer list. DOES NOT WORK

---
+++ Lab1/Customer.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Lab1
+{
+    partial class Customer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameFirst = new System.Windows.Forms.TextBox();
+            this.firstName = new System.Windows.Forms.Label();
+            this.nameLast = new System.Windows.Forms.TextBox();
+            this.lastName = new System.Windows.Forms.Label();
+            this.address = new System.Windows.Forms.Label();
+            this.addressLine = new System.Windows.Forms.TextBox();
+            this.city = new System.Windows.Forms.Label();
+            this.cityLine = new System.Windows.Forms.TextBox();
+            this.state = new System.Windows.Forms.Label();
+            this.stateLine = new System.Windows.Forms.TextBox();
+            this.zip = new System.Windows.Forms.Label();
+            this.zipLine = new System.Windows.Forms.TextBox();
+            this.phone = new System.Windows.Forms.Label();
+            this.phoneLine = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.back = new System.Windows.Forms.Button();
+            this.newCust = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            this.edit = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // nameFirst
+            // 
+            this.nameFirst.Location = new System.Drawing.Point(141, 82);
+            this.nameFirst.Name = "nameFirst";
+            this.nameFirst.Size = new System.Drawing.Size(244, 31);
+            this.nameFirst.TabIndex = 0;
+            // 
+            // firstName
+            // 
+            this.firstName.AutoSize = true;
+            this.firstName.Location = new System.Drawing.Point(13, 85);
+            this.firstName.Name = "firstName";
+            this.firstName.Size = new System.Drawing.Size(122, 25);
+            this.firstName.TabIndex = 1;
+            this.firstName.Text = "First Name:";
+            // 
+            // nameLast
+            // 
+            this.nameLast.Location = new System.Drawing.Point(533, 82);
+            this.nameLast.Name = "nameLast";
+            this.nameLast.Size = new System.Drawing.Size(255, 31);
+            this.nameLast.TabIndex = 2;
+            // 
+            // lastName
+            // 
+            this.lastName.AutoSize = true;
+            this.lastName.Location = new System.Drawing.Point(406, 85);
+            this.lastName.Name = "lastName";
+            this.lastName.Size = new System.Drawing.Size(121, 25);
+            this.lastName.TabIndex = 3;
+            this.lastName.Text = "Last Name:";
+            // 
+            // address
+            // 
+            this.address.AutoSize = true;
+            this.address.Location = new System.Drawing.Point(18, 149);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(97, 25);
+            this.address.TabIndex = 4;
+            this.address.Text = "Address:";
+            // 
+            // addressLine
+            // 
+            this.addressLine.Location = new System.Drawing.Point(121, 146);
+            this.addressLine.Name = "addressLine";
+            this.addressLine.Size = new System.Drawing.Size(666, 31);
+            this.addressLine.TabIndex = 5;
+            // 
+            // city
+            // 
+            this.city.AutoSize = true;
+            this.city.Location = new System.Drawing.Point(18, 206);
+            this.city.Name = "city";
+            this.city.Size = new System.Drawing.Size(55, 25);
+            this.city.TabIndex = 6;
+            this.city.Text = "City:";
+            // 
+            // cityLine
+            // 
+            this.cityLine.Location = new System.Drawing.Point(79, 203);
+            this.cityLine.Name = "cityLine";
+            this.cityLine.Size = new System.Drawing.Size(206, 31);
+            this.cityLine.TabIndex = 7;
+            // 
+            // state
+            // 
+            this.state.AutoSize = true;
+            this.state.Location = new System.Drawing.Point(305, 206);
+            this.state.Name = "state";
+            this.state.Size = new System.Drawing.Size(68, 25);
+            this.state.TabIndex = 8;
+            this.state.Text = "State:";
+            // 
+            // stateLine
+            // 
+            this.stateLine.Location = new System.Drawing.Point(380, 203);
+            this.stateLine.Name = "stateLine";
+            this.stateLine.Size = new System.Drawing.Size(100, 31);
+            this.stateLine.TabIndex = 9;
+            // 
+            // zip
+            // 
+            this.zip.AutoSize = true;
+            this.zip.Location = new System.Drawing.Point(512, 206);
+            this.zip.Name = "zip";
+            this.zip.Size = new System.Drawing.Size(48, 25);
+            this.zip.TabIndex = 10;
+            this.zip.Text = "Zip:";
+            // 
+            // zipLine
+            // 
+            this.zipLine.Location = new System.Drawing.Point(566, 203);
+            this.zipLine.Name = "zipLine";
+            this.zipLine.Size = new System.Drawing.Size(146, 31);
+            this.zipLine.TabIndex = 11;
+            // 
+            // phone
+            // 
+            this.phone.AutoSize = true;
+            this.phone.Location = new System.Drawing.Point(18, 272);
+            this.phone.Name = "phone";
+            this.phone.Size = new System.Drawing.Size(80, 25);
+            this.phone.TabIndex = 12;
+            this.phone.Text = "Phone:";
+            // 
+            // phoneLine
+            // 
+            this.phoneLine.Location = new System.Drawing.Point(105, 269);
+            this.phoneLine.Name = "phoneLine";
+            this.phoneLine.Size = new System.Drawing.Size(280, 31);
+            this.phoneLine.TabIndex = 13;
+            // 
+            // email
+            // 
+            this.email.AutoSize = true;
+            this.email.Location = new System.Drawing.Point(18, 330);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(71, 25);
+            this.email.TabIndex = 14;
+            this.email.Text = "Email:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 327);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(422, 31);
+            this.textBox1.TabIndex = 15;
+            // 
+            // back
+            // 
+            this.back.Location = new System.Drawing.Point(823, 48);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(179, 46);
+            this.back.TabIndex = 16;
+            this.back.Text = "Back";
+            this.back.UseVisualStyleBackColor = true;
+            // 
+            // newCust
+            // 
+            this.newCust.Location = new System.Drawing.Point(823, 100);
+            this.newCust.Name = "newCust";
+            this.newCust.Size = new System.Drawing.Size(179, 48);
+            this.newCust.TabIndex = 17;
+            this.newCust.Text = "New Customer";
+            this.newCust.UseVisualStyleBackColor = true;
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(823, 154);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(179, 47);
+            this.save.TabIndex = 18;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            // 
+            // cancel
+            // 
+            this.cancel.Location = new System.Drawing.Point(823, 208);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(179, 46);
+            this.cancel.TabIndex = 19;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            // 
+            // edit
+            // 
+            this.edit.FormattingEnabled = true;
+            this.edit.Location = new System.Drawing.Point(18, 13);
+            this.edit.Name = "edit";
+            this.edit.Size = new System.Drawing.Size(770, 33);
+            this.edit.TabIndex = 20;
+            // 
+            // Customer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 450);
+            this.Controls.Add(this.edit);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.newCust);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.phoneLine);
+            this.Controls.Add(this.phone);
+            this.Controls.Add(this.zipLine);
+            this.Controls.Add(this.zip);
+            this.Controls.Add(this.stateLine);
+            this.Controls.Add(this.state);
+            this.Controls.Add(this.cityLine);
+            this.Controls.Add(this.city);
+            this.Controls.Add(this.addressLine);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.lastName);
+            this.Controls.Add(this.nameLast);
+            this.Controls.Add(this.firstName);
+            this.Controls.Add(this.nameFirst);
+            this.Name = "Customer";
+            this.Text = "Customer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameFirst;
+        private System.Windows.Forms.Label firstName;
+        private System.Windows.Forms.TextBox nameLast;
+        private System.Windows.Forms.Label lastName;
+        private System.Windows.Forms.Label address;
+        private System.Windows.Forms.TextBox addressLine;
+        private System.Windows.Forms.Label city;
+        private System.Windows.Forms.TextBox cityLine;
+        private System.Windows.Forms.Label state;
+        private System.Windows.Forms.TextBox stateLine;
+        private System.Windows.Forms.Label zip;
+        private System.Windows.Forms.TextBox zipLine;
+        private System.Windows.Forms.Label phone;
+        private System.Windows.Forms.TextBox phoneLine;
+        private System.Windows.Forms.Label email;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.Button newCust;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.ComboBox edit;
+    }
+}
---
+++ Lab1/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Lab1
+{
+    public partial class Customer : Form
+    {
+        public Customer()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
